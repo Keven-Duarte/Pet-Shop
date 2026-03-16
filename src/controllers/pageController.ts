28e19e624ba23f0432ee3fdaa1e7d7f64d7b3c12
Keven-Duarte <Kevenoliveiraduarte2020@gmail.com>
2026-03-16 08:35:44 -0300

    Corrigindo page controller e menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -12,15 +12,30 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
     // res.send('dogs')
-    res.render('pages/page')
+    res.render('pages/page', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 
 export const fishes = (req: Request, res: Response) => {
     // res.send('fishes')
-    res.render('pages/page')
+    res.render('pages/page', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response) => {
     // res.send('cats')
-    res.render('pages/page')
+    res.render('pages/page',{
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
